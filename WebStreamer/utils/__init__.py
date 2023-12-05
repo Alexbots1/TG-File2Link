71462c232b2d22d8f83402e60caacbbe1c9af078
Alexbots1 <119601732+Alexbots1@users.noreply.github.com>
2023-12-05 12:07:02 +0530

    Update __init__.py

--- WebStreamer/utils/__init__.py
+++ WebStreamer/utils/__init__.py
@@ -1,7 +1,1 @@
-# This file is a part of FileStreamBot
-
-from .keepalive import ping_server
-from .time_format import get_readable_time
-from .file_properties import get_name, get_file_ids
-from .custom_dl import ByteStreamer
-tg_connect=""
+# This file is a part of PredatorHackerzZ/TG-File2Link
